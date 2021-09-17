8fed5ab8e4ac637cbf9f3bed94c02306560e0ce2
Rasmus-Balthazar <74965526+Rasmus-Balthazar@users.noreply.github.com>
2021-09-17 12:46:55 +0200

    setup classLib and Student class

---
+++ ClassLib/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+namespace ClassLib
+{
+    public class Student
+    {
+        int ID;
+        string GivenName;
+        string SurName;
+        Status Status;
+        DateTime StartDate;
+        DateTime EndDate;
+        DateTime GraduationDate;
+    }
+}
