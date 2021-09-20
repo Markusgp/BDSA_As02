e23dac5d4a4148759a1b158f64a81afce78f6293
Markusgp <markusgp@live.dk>
2021-09-20 15:09:50 +0200

    Revert "Implemented ImmutableStudent record."
    This reverts commit e2bc614d4224de604df753a46242b9c2c9886da1.

--- ClassLib/Student.cs
+++ ClassLib/Student.cs
@@ -3,7 +3,6 @@ using System.Collections;
 
 namespace ClassLib
 {
-    }
     public class Student
     {
         int ID{get; set;}
@@ -16,7 +15,7 @@ namespace ClassLib
                 DateTime currentDate = DateTime.Now();
                 if(EndDate < GraduationDate)
                 {
-                    //dropout
+                    //droput
                 } 
                 else if(EndDate == GraduationDate)
                 {
@@ -36,57 +35,6 @@ namespace ClassLib
         DateTime EndDate{get; set;}
         DateTime GraduationDate{get; set;}
         public Student(int ID, string GivenName, string SurName, 
-                DateTime StartDate, DateTime EndDate, DateTime GraduationDate)
-        {
-            this.ID = ID;
-            this.GivenName = GivenName;
-            this.SurName = SurName;
-            this.StartDate = StartDate;
-            this.EndDate = EndDate;
-            this.GraduationDate = GraduationDate;
-        }
-
-        public override string ToString()
-        {
-            string toString = "ID: " + Student.id + " " + Student.GivenName + " " + 
-            Student.SurName + " " + "insert status" + " " + Student.StartDate + " " + Student.EndDate 
-            + " " + Student.GraduationDate;
-            return toString;
-        }
-    }
-    
-
-    public record ImmutableStudent {
-        int ID{get; init;}
-        string GivenName{get; init;}
-        string SurName{get; init;}
-        Status Status
-        {
-            get
-            {
-                DateTime currentDate = DateTime.Now();
-                if(EndDate < GraduationDate)
-                {
-                    //dropout
-                } 
-                else if(EndDate == GraduationDate)
-                {
-                    //Graduated?
-                } 
-                else if(StartDate == currentDate)
-                {
-                    //new
-                } else
-                {
-                    //active
-                }
-                
-            }
-        }
-        DateTime StartDate{get; init;}
-        DateTime EndDate{get; init;}
-        DateTime GraduationDate{get; init;}
-        public ImmutableStudent(int ID, string GivenName, string SurName, 
                 DateTime StartDate, DateTime EndDate, DateTime GraduationDate)
         {
             this.ID = ID;
